589a34106a8c565d2cfbdcb1c7ca3047116d57f5
naoyuki <nfrutta1208@gmail.com>
2021-08-21 18:54:16 +0900

    today画面実装中

--- project/simple_timecard/lib/today/today_model.dart
+++ project/simple_timecard/lib/today/today_model.dart
@@ -1,3 +1,22 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 
-class TodayModel extends ChangeNotifier {}
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+
+class TodayModel extends ChangeNotifier {
+  Timer? _timer;
+  String datetimeString = '';
+
+  void startTimer() {
+    var a = DateTime.now();
+    datetimeString = DateFormat('yyyy/MM/dd hh:mm:ss').format(a);
+    _timer = Timer.periodic(Duration(seconds: 1), _onTimer);
+    notifyListeners();
+  }
+
+  void _onTimer(Timer timer) {
+    var a = DateTime.now();
+    datetimeString = DateFormat('yyyy/MM/dd hh:mm:ss').format(a);
+    notifyListeners();
+  }
+}
--- project/simple_timecard/lib/today/today_page.dart
+++ project/simple_timecard/lib/today/today_page.dart
@@ -7,12 +7,86 @@ class TodayPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<TodayModel>(
-      create: (_) => TodayModel(),
+      create: (_) => TodayModel()..startTimer(),
       child: Scaffold(
-        backgroundColor: Colors.amber,
         body: Center(
           child: Consumer<TodayModel>(builder: (context, model, child) {
-            return Text('Today Page.');
+            return Column(
+              children: [
+                SizedBox(
+                  height: 48,
+                ),
+                Text(
+                  model.datetimeString,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: 48,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '出勤時刻：',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      '09:00',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '退勤時刻：',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      '18:00',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 48,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            print('click!');
+                            // TODO
+                          },
+                          child: Text('出勤登録')),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            print('click!');
+                            // TODO
+                          },
+                          child: Text('退勤登録')),
+                    ),
+                  ],
+                ),
+              ],
+            );
           }),
         ),
       ),
