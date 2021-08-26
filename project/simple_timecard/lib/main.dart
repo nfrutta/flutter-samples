b019ce0c90d5031d35fb97159eb5c435b9d1064c
naoyuki <nfrutta1208@gmail.com>
2021-08-26 14:30:42 +0900

    local設定

--- project/simple_timecard/lib/main.dart
+++ project/simple_timecard/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'app.dart';
 
 void main() async {
+  initializeDateFormatting('ja');
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
