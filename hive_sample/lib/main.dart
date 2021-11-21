cfda39c4357308a1661e7a5ae28842964326e7e0
naoyuki <nfrutta1208@gmail.com>
2021-11-21 22:09:47 +0900

    パッケージ追加、ビルド通ることを確認

--- hive_sample/lib/main.dart
+++ hive_sample/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('sample');
   runApp(const MyApp());
 }
 
