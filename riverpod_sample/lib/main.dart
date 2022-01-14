3e1987cd6e9b2a50aad2678420994ebe66afbf75
naoyuki <nfrutta1208@gmail.com>
2022-01-14 22:28:25 +0900

    stableリリース後の変更を反映

--- riverpod_sample/lib/main.dart
+++ riverpod_sample/lib/main.dart
@@ -1,56 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'main_model.dart';
+import 'counter.dart';
+
+final counterProvider = StateNotifierProvider((ref) => Counter());
 
 void main() {
   runApp(
-    const ProviderScope(
+    ProviderScope(
       child: MyApp(),
     ),
   );
 }
 
-final counterProvider =
-    ChangeNotifierProvider<CountModel>((ref) => CountModel());
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    final countModel = context.read(counterProvider);
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: SampleWidget(),
+    );
+  }
+}
+
+class SampleWidget extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'クリックの回数',
+            ),
+            Text('$count'),
+          ],
+        ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Riverpod サンプル'),
-        ),
-        body: Center(
-          child: Consumer(builder: (context, watch, _) {
-            final counter = watch(counterProvider).count;
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'クリックの回数',
-                ),
-                Text(
-                  '$counter',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ],
-            );
-          }),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: countModel.incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(counterProvider.notifier).increment();
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
